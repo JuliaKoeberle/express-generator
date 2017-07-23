1f5f4b5c5d5bc2a9c21cc465612bb9c1364e59ce
Julia <julia.f.koeberle@gmail.com>
2017-07-23 15:55:39 -0400

    simple express server with express generator

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const Validator = require('validator');
+const isEmpty = require('lodash/isEmpty');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.post('/', function(req, res, next) {
+  let errors = {};
+
+  //verify all fields
+  if(Validator.isEmpty(req.body.email)){
+    errors.email = 'Email is required';
+  }
+  function validateInput(data){
+    return {
+      errors, 
+      isValid: isEmpty(errors)
+    }
+  }
+  const {err, isValid} = validateInput(req.body);
+
+  if(!isValid){
+    res.status(400).send({errors: errors});
+  }
+
+});
+
+module.exports = router;
